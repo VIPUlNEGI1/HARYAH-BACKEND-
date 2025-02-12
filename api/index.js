5bc0772611651276e5d64a3ae1534a84492ce3d0
vipulnehi <99vipul88@gmail.com>
2025-02-12 12:42:12 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://hayah-frontend.vercel.app'
+  }));
 app.use(bodyParser.json());
 
 // Routes
